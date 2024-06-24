26961807af43c3c507050e7c2ce81bb3b4179473
Gvantsie <g_euashvili@cu.edu.ge>
2024-06-24 22:17:33 +0400

    https error solved

--- Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+645c3fd92dc051dafde8340ea7587ad46844d4a9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3b2c45beef938dd60ea0785538055571eadf8f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
